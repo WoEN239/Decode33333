7154b4359c1adc1e0bc0836157f34d385178f7ee
GetGridderr <krime@krime.krime>
2025-10-22 00:15:44 +0300

    added class for working with Gun.

---
+++ TeamCode/src/main/java/org/woen/main/movement/SimpleTrajectoryMovement.java
@@ -0,0 +1,9 @@
+package org.woen.main.movement;
+
+import org.woen.main.movement.Vehicles;
+
+public class SimpleTrajectoryMovement {
+    private double xPosition, yPosition;
+    private double acceleration, Velocity;
+    private Vehicles base;
+}
--- TeamCode/src/main/java/org/woen/main/revolver/Gun.java
+++ TeamCode/src/main/java/org/woen/main/revolver/Gun.java
@@ -5,16 +5,19 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.woen.core.device.motor.Motor;
 import org.woen.core.device.trait.Initializable;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 public final class Gun implements Initializable {
     private static final Gun INSTANCE = new Gun();
+    private double velocity = 0;
+    private final Motor motorLeft;
+    private final Motor motorRight;
 
-    private final Motor motor;
 
-
-    private Gun() {
-        motor = new Motor("gun_motor");
+    public Gun() {
+        motorLeft = new Motor("gun_motor_left");
+        motorRight = new Motor("gun_motor_right");
     }
 
     public static Gun getInstance() {
@@ -23,11 +26,35 @@ public final class Gun implements Initializable {
 
     @Override
     public void initialize(HardwareMap hardwareMap) {
-        motor.initialize(hardwareMap);
+        motorLeft.initialize(hardwareMap);
+        motorRight.initialize(hardwareMap);
     }
 
     @Override
     public boolean isInitialized() {
-        return motor.isInitialized();
+
+        return motorLeft.isInitialized() && motorRight.isInitialized();
+    }
+
+    public void invertDirection(Motor motor) {
+        motor.setDirection(motor.getDirection().inverted());
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+        motorLeft.setVelocity(velocity);
+        motorRight.setVelocity(velocity);
+    }
+
+    public double getVelocity() { return velocity; }
+
+    public void startShot() {
+        motorRight.setPower(motorLeft.getVelocity());
+        motorLeft.setPower(motorLeft.getVelocity());
+    }
+
+    public void stopShoot() {
+        motorRight.stopMotor();
+        motorLeft.stopMotor();
     }
 }
