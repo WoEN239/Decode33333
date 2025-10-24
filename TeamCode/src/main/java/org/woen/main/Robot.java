40a151230e23d74cca46234cbb34a52d237d2310
cve0000 <is.void.is.none@gmail.com>
2025-10-24 22:26:17 +0300

    Change Robot #static class to singleton object

--- TeamCode/src/main/java/org/woen/main/Robot.java
+++ TeamCode/src/main/java/org/woen/main/Robot.java
@@ -1,16 +1,43 @@
 package org.woen.main;
 
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.woen.core.device.trait.Initializable;
 import org.woen.main.gun.GunControl;
 import org.woen.main.movement.Vehicles;
 
 
-public final class Robot {
-    public static Vehicles getVehiclesController() {
+public final class Robot implements Initializable {
+    private static final Robot INSTANCE = new Robot();
+
+    boolean initialized;
+
+
+    private Robot() {
+        initialized = false;
+    }
+
+    public Vehicles getVehiclesController() {
         return Vehicles.getInstance();
     }
 
-    public static GunControl getGunController() {
+    public GunControl getGunController() {
         return GunControl.getInstance();
+    }
+
+    @Override
+    public void initialize(HardwareMap hardwareMap) {
+        if (isInitialized()) return;
+
+        getVehiclesController().initialize(hardwareMap);
+        getGunController().initialize(hardwareMap);
+
+        initialized = true;
+    }
+
+    @Override
+    public boolean isInitialized() {
+        return initialized;
     }
 }
