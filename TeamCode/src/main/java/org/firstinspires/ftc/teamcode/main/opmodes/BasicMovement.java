8ed2dcb94d10227781dcaa2df7f7b53dc47d5292
xTimop <twistomin@gmail.com>
2025-11-15 18:18:31 +0300

    Remove unused imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/opmodes/BasicMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/opmodes/BasicMovement.java
@@ -1,19 +1,9 @@
 package org.firstinspires.ftc.teamcode.main.opmodes;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.teamcode.core.device.odometer.Odometer;
 import org.firstinspires.ftc.teamcode.core.device.single.Gyro;
 import org.firstinspires.ftc.teamcode.main.movement.Odometry;
 import org.firstinspires.ftc.teamcode.main.movement.Vehicles;
